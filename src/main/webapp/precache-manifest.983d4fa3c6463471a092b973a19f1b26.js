09f5216906cffe29eded7e1e3af7fe7373db2502
meier.i@husky.neu.edu <43007421+meieri@users.noreply.github.com>
2020-06-20 23:59:51 -0400

    added client

---
+++ src/main/webapp/precache-manifest.983d4fa3c6463471a092b973a19f1b26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5989b3f029308fbf86b0bf82dde37dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "836067ddeaf535c1d404",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "561edfbd9a6007bc89e9",
+    "url": "/static/css/main.ecdc85d9.chunk.css"
+  },
+  {
+    "revision": "836067ddeaf535c1d404",
+    "url": "/static/js/2.08072cde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08072cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561edfbd9a6007bc89e9",
+    "url": "/static/js/main.0758963f.chunk.js"
+  },
+  {
+    "revision": "ed149c6d39d23a26ae77",
+    "url": "/static/js/runtime-main.35d2e575.js"
+  },
+  {
+    "revision": "0847227b3c5baa567b6630e28f8cc0d8",
+    "url": "/static/media/Sriracha-Regular.0847227b.ttf"
+  },
+  {
+    "revision": "3f80d8d7cbdde823f91faa1cac3122fc",
+    "url": "/static/media/game-show-host-1.3f80d8d7.png"
+  },
+  {
+    "revision": "ed80e29a60840c6ebb40e079ca5e7af4",
+    "url": "/static/media/no_continue.ed80e29a.ttf"
+  }
+]);
